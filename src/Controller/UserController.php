2c929dbe9dd57518040350961224a752f216a1bf
Igor Kusmitsch <igor.kusmitsch@gmail.com>
2018-07-16 15:45:28 -0300

    Entidade e webService User

---
+++ src/Controller/UserController.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Utils\Autenticar;
+use App\Entity\User;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class UserController extends Controller
+{
+
+    public function insert(Request $request, Autenticar $autenticar, TranslatorInterface $translator)
+    {
+
+        try{
+            if($autenticar->token($request->headers->get('token'))){
+
+                if( empty($request->get('password')) || $request->get('password') == NULL) {
+                    throw new \TypeError("parameter [password]: " . $translator->trans('empty'));
+                }
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $user = new User();
+                $user->setRg($request->get('rg'))
+                        ->setFullName($request->get('full_name'))
+                        ->setEmail($request->get('email'))
+                        ->setNickName($request->get('nickname'))
+                        ->setPassword(hash('sha256', $request->get('password')));
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+                return new JsonResponse(['status' => $translator->trans('success'), 'response' => $translator->trans('insert')]);
+            }else{
+                return new JsonResponse(['status' => $translator->trans('error'), 'response' => $translator->trans('insert')]); 
+            }
+        }catch(\TypeError | \Doctrine\DBAL\Exception\UniqueConstraintViolationException  $ex){
+            return new JsonResponse(['status' => $translator->trans('error'), 'response' => $ex->getmessage()]);
+        }
+    }
+
+
+    public function validate(Request $request, TranslatorInterface $translator)
+    {
+
+        try{
+            $user = $this->getDoctrine()->getRepository(User::class)->findByEmailOrNickName($request->headers->get('nickname'), $request->headers->get('password'));
+
+            if(!$user){
+                throw new \Doctrine\DBAL\Exception\InvalidArgumentException($translator->trans('not_found'));
+            }else{
+                return new JsonResponse(['status' => $translator->trans('success'), 'response' => true]);
+            }
+        }catch(\TypeError | \Doctrine\DBAL\Exception\UniqueConstraintViolationException  $ex){
+            return new JsonResponse(['status' => $translator->trans('error'), 'response' => $ex->getmessage()]);
+        }
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="string", length=13)
+     */
+    private $rg;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $fullName;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=12)
+     */
+    private $nickname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    public function getRg(): ?string
+    {
+        return $this->rg;
+    }
+
+    public function setRg(string $rg): self
+    {
+        $this->rg = $rg;
+
+        return $this;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->fullName;
+    }
+
+    public function setFullName(string $fullName): self
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): self
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
