dd26be61803ec6ce7ca43feba9f8c895991b0d96
Igor Kusmitsch <igor.kusmitsch@gmail.com>
2018-07-17 18:28:25 -0300

    Add Class UserRepository

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+
+    /**
+     * @return User[] Returns an array of User objects
+     */
+    public function findByEmailOrNickName(string $value, string $password)
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.nickname = :value OR u.email = :value')
+            ->andWhere('u.password = :password')
+            ->setParameter('value', $value)
+            ->setParameter('password', $password)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+}
