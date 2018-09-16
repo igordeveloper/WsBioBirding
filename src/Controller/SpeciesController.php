021b76c17cf5e27f92cf8419f5f8da1993a43322
Igor Kusmitsch <igor.kusmitsch@gmail.com>
2018-09-16 12:02:53 -0300

    SpeciesController

--- src/Controller/SpeciesController.php
+++ src/Controller/SpeciesController.php
@@ -143,14 +143,14 @@ class SpeciesController extends AbstractController
 
                     $species->setScientificName($request->get("scientificName"));
 
-                    if( empty($request->get("notes")) || is_null($request->get("notes")) ){
+                    if( empty($request->get("notes"))){
                         $species->setNotes(NULL);
                     }else{
                         $species->setNotes($request->get("notes"));
                     }
 
 
-                    if( empty($request->get("conservationState")) || is_null($request->get("conservationState")) ){
+                    if( empty($request->get("conservationState"))){
                         $species->setConservationState(NULL);
                     }else{
                         $species->setConservationState($request->get("conservationState"));
