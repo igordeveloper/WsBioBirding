e9ef9976cf00b33d8db0fcfc1d85a1d999554670
Igor Kusmitsch <igor.kusmitsch@gmail.com>
2018-09-16 11:23:00 -0300

    SpeciesController

--- src/Controller/SpeciesController.php
+++ src/Controller/SpeciesController.php
@@ -14,130 +14,144 @@ use Symfony\Component\Translation\TranslatorInterface;
 class SpeciesController extends AbstractController
 {
 
-    public function insert(Request $request, AutenticateHelper $autenticate, TranslatorInterface $translator)
+    public function insert(Request $request, AutenticateHelper $autenticate)
     {
 
         try{
-            if($autenticate->verify($request->headers->get('authorizationCode'))){
-                $entityManager = $this->getDoctrine()->getManager();
+            if($autenticate->verify($request->headers->get("authorizationCode"))){
+
                 $species = new Species();
-                $species->setScientificName($request->get('scientificName'));
-                $species->setNotes($request->get('notes'));
-                $species->setConservationState($request->get('conservationState'));
+                $species->setScientificName($request->get("scientificName"));
+                $species->setNotes($request->get("notes"));
+                $species->setConservationState($request->get("conservationState"));
+
+                $entityManager = $this->getDoctrine()->getManager();
                 $entityManager->persist($species);
                 $entityManager->flush();
-                return new JsonResponse(['authorized' => true, 'response' => $translator->trans('insert')]);
+
+                return new JsonResponse(["authorized" => true, "response" => true]);
             }else{
-                return new JsonResponse(['authorized' => false]); 
+                return new JsonResponse(["authorized" => false]); 
             }
         }catch(\TypeError | \Doctrine\DBAL\Exception\UniqueConstraintViolationException  $ex){
-            return new JsonResponse(['exception' => $ex->getmessage()]);
+            return new JsonResponse(["exception" => $ex->getmessage()]);
         }
     }
 
 
-    public function search(Request $request, AutenticateHelper $autenticate, TranslatorInterface $translator)
+    public function search(Request $request, AutenticateHelper $autenticate)
     {
 
         try{
-            if($autenticate->verify($request->headers->get('authorizationCode'))){
-                $species = $this->getDoctrine()->getRepository(Species::class)->findByScientificName($request->get('scientificName'));
-                $list = array();
+            if($autenticate->verify($request->headers->get("authorizationCode"))){
 
+                $species = $this->getDoctrine()->getRepository(Species::class)
+                            ->findByScientificName($request->get("scientificName"));
+
+                $list = array();
 
                 foreach ($species as $specie) {
 
-
                     $list[] = array(
-                                    'id' => $specie->getId(), 
-                                    'scientificName' => $specie->getScientificName()
-                                    );         
+                        "id" => $specie->getId(), 
+                        "scientificName" => $specie->getScientificName()
+                    );
+
                 }
-                return new JsonResponse(['authorized' => true , 'species' => $list]);
+
+                return new JsonResponse(["authorized" => true , "species" => $list]);
             }else{
-                return new JsonResponse(['authorized' => false, 'response' => $translator->trans('not_authorized')]); 
+                return new JsonResponse(["authorized" => false]); 
             }
         }catch(\TypeError $ex){
-            return new JsonResponse(['exception' => $ex->getmessage()]);
+            return new JsonResponse(["exception" => $ex->getmessage()]);
         }
     }
 
 
-    public function select(Request $request, AutenticateHelper $autenticate, TranslatorInterface $translator)
+    public function select(Request $request, AutenticateHelper $autenticate)
     {
 
         try{
-            if($autenticate->verify($request->headers->get('authorizationCode'))){
-                $species = $this->getDoctrine()->getRepository(Species::class)->find($request->get('id'));
+            if($autenticate->verify($request->headers->get("authorizationCode"))){
+
+                $species = $this->getDoctrine()->getRepository(Species::class)
+                            ->find($request->get("id"));
                 
                 if($species){
                     $list = array(
-                            'scientificName' => $species->getScientificName(), 
-                            'notes' => $species->getNotes(),
-                            'conservationState' => $species->getConservationState(),
-                            );  
+                        "scientificName" => $species->getScientificName(), 
+                        "notes" => $species->getNotes(),
+                        "conservationState" => $species->getConservationState(),
+                    );  
                 }else{
                     $list = NULL;
                 }
 
-                return new JsonResponse(['authorized' => true , 'species' => $list]);
+                return new JsonResponse(["authorized" => true , "species" => $list]);
             }else{
-                return new JsonResponse(['authorized' => false, 'response' => $translator->trans('not_authorized')]); 
+                return new JsonResponse(["authorized" => false]); 
             }
         }catch(\TypeError $ex){
-            return new JsonResponse(['exception' => $ex->getmessage()]);
+            return new JsonResponse(["exception" => $ex->getmessage()]);
         }
     }
 
 
-    public function selectAll(Request $request, AutenticateHelper $autenticate, TranslatorInterface $translator)
+    public function selectAll(Request $request, AutenticateHelper $autenticate)
     {
 
         try{
-            if($autenticate->verify($request->headers->get('authorizationCode'))){
-                $species = $this->getDoctrine()->getRepository(Species::class)->findAll();
+            if($autenticate->verify($request->headers->get("authorizationCode"))){
+
+                $species = $this->getDoctrine()->getRepository(Species::class)
+                            ->findAll();
+
                 $list = array();
 
                 foreach ($species as $specie) {
 
-
                     $list[] = array(
-                                    'id' => $specie->getId(), 
-                                    'scientificName' => $specie->getScientificName()
-                                    );         
+                        "id" => $specie->getId(), 
+                        "scientificName" => $specie->getScientificName()
+                    );         
                 }
 
-                return new JsonResponse(['authorized' => true , 'species' => $list]);
+                return new JsonResponse(["authorized" => true , "species" => $list]);
             }else{
-                return new JsonResponse(['authorized' => false, 'response' => $translator->trans('not_authorized')]); 
+                return new JsonResponse(["authorized" => false]); 
             }
         }catch(\TypeError $ex){
-            return new JsonResponse(['exception' => $ex->getmessage()]);
+            return new JsonResponse(["exception" => $ex->getmessage()]);
         }
     }
 
 
-    public function update(Request $request, AutenticateHelper $autenticate, TranslatorInterface $translator)
+    public function update(Request $request, AutenticateHelper $autenticate)
     {
 
         try{
-            if($autenticate->verify($request->headers->get('authorizationCode'))){
+            if($autenticate->verify($request->headers->get("authorizationCode"))){
+
                 $entityManager = $this->getDoctrine()->getManager();
-                $species = $entityManager->getRepository(Species::class)->find($request->get('id'));
+                $species = $entityManager->getRepository(Species::class)
+                            ->find($request->get("id"));
 
                 if(!$species) {
-                    throw new \Doctrine\DBAL\Exception\InvalidArgumentException($translator->trans('not_found'));
+                    throw new \Doctrine\DBAL\Exception\InvalidArgumentException("Not found");
                 }else{
-                    $species->setScientificName($request->get('scientificName'));
-                    $species->setNotes($request->get('notes'));
-                    $species->setConservationState($request->get('conservationState'));
+                    
+                    $species->setScientificName($request->get("scientificName"));
+                    $species->setNotes($request->get("notes"));
+                    $species->setConservationState($request->get("conservationState"));
                     $entityManager->flush();
-                  return new JsonResponse(['authorized' => true, 'response' => $translator->trans('update')]);
+
+                    return new JsonResponse(["authorized" => true, "response" => true]);
 
                 }
             }
         }catch(\TypeError |  \Doctrine\DBAL\Exception\UniqueConstraintViolationException | \Doctrine\DBAL\Exception\InvalidArgumentException | \Doctrine\ORM\ORMException $ex){
-            return new JsonResponse(['exception' => $ex->getmessage()]);
+            return new JsonResponse(["exception" => $ex->getmessage()]);
         }
     }
 
