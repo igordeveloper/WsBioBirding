d60946948a04d50fcffcb8a3adc6714c114b5497
Igor Kusmitsch <igor.kusmitsch@gmail.com>
2018-09-26 01:17:02 -0300

    WeatherHelper

--- src/Helper/WeatherHelper.php
+++ src/Helper/WeatherHelper.php
@@ -14,7 +14,7 @@ class WeatherHelper
     public function check(float $latitude, float $longitude, float $timestamp): array
     {
 
-        $ch = curl_init();
+        $ch = \curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL, "https://api.darksky.net/forecast/".$this->secretKeyDarkSky."/".$latitude.",".$longitude.",".$timestamp."?exclude=hourly,%20flags,alerts&lang=pt&units=ca");
