bdaca184d8a912a59969cd17ce45216f60bc0f06
Igor Kusmitsch <igor.kusmitsch@gmail.com>
2018-10-12 11:11:27 -0300

    Entidade Log

---
+++ src/Entity/Log.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LogRepository")
+ */
+class Log
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $action;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $timestamp;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAction(): ?string
+    {
+        return $this->action;
+    }
+
+    public function setAction(string $action): self
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    public function getTimestamp(): ?int
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(int $timestamp): self
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+}
