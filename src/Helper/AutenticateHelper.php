6872a42eedb505c31bfcf3a3396bfa922ca541fc
Igor Kusmitsch <igor.kusmitsch@gmail.com>
2018-11-17 12:15:00 -0200

    helper

--- src/Helper/AutenticateHelper.php
+++ src/Helper/AutenticateHelper.php
@@ -20,7 +20,7 @@ class AutenticateHelper
         $authorization = base64_decode($authorization);
         $temporary = explode("||", $authorization);
 
-        $user = $this->em->getRepository(User::class)->checkToken($temporary[0], $temporary[1]);
+        $user = $this->em->getRepository(User::class)->checkToken($temporary[0], $temporary[1], $temporary[2]);
          if($user){
             return true;
          }else{
